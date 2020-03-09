b52caae2a9723705e9d44e4a0b8cc0d64cec9fb7
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-03-09 22:23:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'config/environment'
+require 'pry '
 
 class App < Sinatra::Base
   
@@ -21,6 +22,6 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
     @word = params[:word]
     binding.pry 
-    "#{"
+  end 
 
 end

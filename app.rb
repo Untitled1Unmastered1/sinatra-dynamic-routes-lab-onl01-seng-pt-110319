185a55ba12b84555f2070d433779f52ee5b83c64
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-03-09 22:39:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,7 +31,9 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1].to_i 
     @number2 = params[:number2].to_i
-    case 
-    when 
+    case @operation
+    when "add"
+      "#{@number1 + @number2}"
+      when 
 
 end
